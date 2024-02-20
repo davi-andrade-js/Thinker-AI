ee90eae67ece437be23556fc578f167aa945baf6
Davi <dv.andrade04@gmail.com>
2024-02-19 22:21:22 -0300

    working on sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard page (Protected)</p>;
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
